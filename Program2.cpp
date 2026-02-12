4abbb9f3676dd1e51ad340da89b368a35d4330d8
Anirudh Gupta <surisettyanirudh@gmail.com>
2026-02-12 22:46:55 +0530

    input and output

---
+++ Program2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=0;
+    int b=0;
+    cout<<"Enter value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter value of b: "<<endl;
+    cin>>b;
+    cout<<"Sum of a and b is: "<<a+b<<endl;
+
+    return 0;
+}
